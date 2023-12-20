345bcaa7a43a15a8acd69a54ef891c7218ce707d
Yash <pauranik51@gmail.com>
2023-12-20 17:53:57 +0530

    chore: First commit to check

---
+++ pages/calculators/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Calcultors home page</h1>
+    </div>
+</template>
+
+<script></script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <nuxt-link to="/calculators/">Calculators</nuxt-link>
+        <nuxt-link to="/">Home</nuxt-link>
+        <slot/>
+    </div>
+</template>
+
+<script></script>
